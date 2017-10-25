d46856d1296f19fa5abe2605aa89ccc3b842c183
Jari Zwarts <jarizw@gmail.com>
2017-10-25 16:58:38 +0200

    Full build script, animations, dancing babies

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# variables to play with
+out="images/W95-MOD.IMG"
+shadow="/tmp/jari.shadow"
+mount="./w95"
+
 echo "Generating custom w95 image..."
 
 if [ ! -f ./images/W95.IMG ]; then
@@ -8,7 +13,24 @@ if [ ! -f ./images/W95.IMG ]; then
 fi
 
 echo "Mounting..."
-hdiutil attach images/W95.IMG -mountpoint ./w95 -shadow /tmp/jari.shadow -noverify
+diskinfo=$(hdiutil attach images/W95.IMG -mountpoint ${mount} -shadow ${shadow} -noverify)
+device=$(echo $diskinfo | cut -d' ' -f1)
+echo "Done. Mounted as: $device"
 
-echo "Copying files to disk..."
+echo "Copying files to mounted disk..."
 cp -R disk w95
+
+echo "Unmounting sector 1..."
+diskutil unmount ${device}s1
+
+echo "Writing image to $out"
+dd if=${device} of=${out}
+
+echo "Cleaning up..."
+rm ${shadow}
+
+echo "Ejecting..."
+diskutil unmountDisk ${device}
+diskutil eject ${device}
+
+echo "Complete! Find your image @ '${out}'"
---
+++ js/init.js
@@ -0,0 +1,96 @@
+window.onload = function () {
+    var os_uses_mouse = false;
+    var useState = window.location.search !== '?forceDisk';
+
+    var settings = {
+        memory_size: 32 * 1024 * 1024,
+        screen_container: document.getElementById('screen_container'),
+        hda: {
+            url: 'images/W95-MOD.IMG',
+            async: useState,
+            size: 231 * 1024 * 1024,
+        },
+        autostart: true,
+    };
+
+    if (useState) {
+        settings.initial_state = {
+            url: './images/v86state.bin',
+            size: 41168,
+        };
+    } else {
+        settings = Object.assign(settings, {
+            bios: {
+                url: './bios/seabios.bin',
+            },
+            vga_bios: {
+                url: './bios/vgabios.bin',
+            },
+        });
+    }
+
+    const emulator = window.emulator = new V86Starter(settings);
+
+    emulator.add_listener('mouse-enable', function (is_enabled) {
+        os_uses_mouse = is_enabled;
+    });
+
+    document.getElementById('screen_container').onclick = function () {
+        emulator.lock_mouse();
+        emulator.screen_go_fullscreen();
+        console.log('stuiter met die billen als skippy bal')
+
+        // todo mobile stuff
+    };
+
+    function dump_file(ab, name)
+    {
+        if(!(ab instanceof Array))
+        {
+            ab = [ab];
+        }
+
+        var blob = new Blob(ab);
+        download(blob, name);
+    }
+
+    function download(file_or_blob, name)
+    {
+        var a = document.createElement('a');
+        a['download'] = name;
+        a.href = window.URL.createObjectURL(file_or_blob);
+        a.dataset['downloadurl'] = ['application/octet-stream', a['download'], a.href].join(':');
+
+        if(document.createEvent)
+        {
+            var ev = document.createEvent('MouseEvent');
+            ev.initMouseEvent('click', true, true, window,
+                0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            a.dispatchEvent(ev);
+        }
+        else
+        {
+            a.click();
+        }
+
+        window.URL.revokeObjectURL(a.href);
+    }
+
+    document.getElementById('dumpstate').onclick = () =>
+        emulator.save_state(function(error, result)
+        {
+            if(error)
+            {
+                console.log(error.stack);
+                console.log('Couldn\'t save state: ', error);
+            }
+            else
+            {
+                dump_file(result, 'v86state.bin');
+            }
+        });
+
+    emulator.add_listener('screen-set-mode', (isGraphic) => {
+        isGraphic && document.getElementById('screen_container').classList.add('online')
+    })
+};
